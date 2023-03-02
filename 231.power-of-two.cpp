424037b730cd6731a7e8e9914accd2ebd4aa9638
Danieldu <daniel@jasslin.com>
2023-03-02 16:10:34 +0800

    add 231

---
+++ 231.power-of-two.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=231 lang=cpp
+ *
+ * [231] Power of Two
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        for (int  i = 0; i < 31; i++)
+        {
+            if (1<<i==n)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
+// @lc code=end
+
