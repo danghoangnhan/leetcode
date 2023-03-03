09af45a110b00ffbe8a846c74d3f15f33b1013e2
danieldu <danghoangnhan.1@gmail.com>
2023-03-03 13:38:54 +0800

    add 344

---
+++ 344.reverse-string.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+/*
+ * @lc app=leetcode id=344 lang=cpp
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(), s.end());
+    }
+};
+// @lc code=end
+
