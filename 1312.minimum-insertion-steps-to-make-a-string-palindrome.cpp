bd464626b8a205a4df4a43434b630308b7eba3f2
danieldu <danghoangnhan.1@gmail.com>
2023-03-06 14:01:19 +0800

    add 1312

---
+++ 1312.minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,36 @@
+#include<string>
+#include <algorithm> 
+
+using namespace std;
+/*
+ * @lc app=leetcode id=1312 lang=cpp
+ *
+ * [1312] Minimum Insertion Steps to Make a String Palindrome
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+
+    int length[text1.size()+1][text2.size()+1];
+	
+    for (int i=0; i<=text1.size(); i++) length[i][0] = 0;
+	for (int j=0; j<=text2.size(); j++) length[0][j] = 0;
+
+	for (int i=1; i<=text1.size(); i++)
+		for (int j=1; j<=text2.size(); j++)
+			if (text1[i-1] == text2[j-1])
+				length[i][j] = length[i-1][j-1] + 1;
+			else
+				length[i][j] = max(length[i-1][j],length[i][j-1]);
+	return length[text1.size()][text2.size()];
+    }
+    int minInsertions(string s) {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        return  s.size() - longestCommonSubsequence(s,s2);
+    }
+};
+// @lc code=end
+
