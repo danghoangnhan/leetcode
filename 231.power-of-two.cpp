cc54759396e67a621394fa7e3af8c0fa229e7065
Danieldu <daniel@jasslin.com>
2023-03-08 14:47:10 +0800

    complete 231

--- 231.power-of-two.cpp
+++ 231.power-of-two.cpp
@@ -9,15 +9,8 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         for (int  i = 0; i < 31; i++)
-        {
-            if (1<<i==n)
-            {
-                return true;
-            }
-        }
+            if (1<<i==n)return true;
         return false;
-        
     }
 };
 // @lc code=end
-
