63f105239c827f78a438406ad20ac21c1683b9b3
danieldu <danghoangnhan.1@gmail.com>
2023-03-11 13:38:10 +0800

    add 136

---
+++ 136.single-number.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+/*
+ * @lc app=leetcode id=136 lang=cpp
+ *
+ * [136] Single Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            result ^=nums[i];
+        }
+    return result;        
+    }
+};
+// @lc code=end
+
