b260caf99102d69d3a8c07e98be379f9dcec005f
danieldu <danghoangnhan.1@gmail.com>
2023-03-12 17:46:40 +0800

    continouse

--- 139.word-break.cpp
+++ 139.word-break.cpp
@@ -1,5 +1,6 @@
 #include<string>
 #include<vector>
+#include<set>
 #define FOR(i, L, R) for(int i = L; i < (int)R; ++i)
 #define FORD(i, L, R) for(int i = L; i > (int)R; --i)
 using namespace std;
@@ -8,56 +9,79 @@ using namespace std;
  *
  * [139] Word Break
  */
-struct Node
-{
-    char ch;
-    int v;
+class Node{
+public:
+    bool isWord;
     Node *next[26];
-    Node()
-    {
-        v = 0;
-        FOR(i, 0, 26) next[i] = NULL;
+    Node(){
+        for (auto &a : next) a = nullptr;
+        isWord = false;
     }
 };
-void insert(Node *root, string s)
-{
-    FOR(i, 0, s.size())
-    {
-        int v = s[i] - 'a';
-        if (root->next[v] == NULL)
-        {
-            root->next[v] = new Node();
-        }
-        root = root->next[v];
-        ++root->v;
-        root->ch = s[i];
+class Trie {
+private:
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
     }
-    return;
-}
-void search(Node *root, string s)
-{
-    FOR(i, 0, s.size())
-    {
-        int v = s[i] - 'a';
-        root = root->next[v];
-        if (root->v == 1)
-        {
-            cout << s << ' ' << s.substr(0, i + 1) << '\n';
-            return;
+    
+    void insert(string word) {
+        Node* current = this->root;
+        for(char x:word){
+            int v = x - 'a';
+            if (!current->next[v])current->next[v] = new Node();
+            current = current->next[v];
         }
+        current->isWord = true;
     }
-    cout << s << ' ' << s << '\n';
-}
+    
+    bool search(string word) {
+        Node* current = this->root;
+        for (auto &a : word) {
+            int v = tolower(a) - 'a';
+            if (!current->next[v])return false;
+            current = current->next[v];
+        }
+        return current->isWord;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* current = this->root;
+        for (auto &a : prefix) {
+            int v = a - 'a';
+            if (!current->next[v])return false;
+                current = current->next[v];
+        }
+        return true;
+    }
+};
+
 
 // @lc code=start
 class Solution {
+private:
+    bool visited[300] = {};
+    string s;
+    Trie* trie;
+    set<string> dict;
+    bool dfs(int i) {
+            if (i == size(s))return true;
+            if (!visited[i]) {
+                visited[i] = true;
+                for (int j = i; j < size(s); j++) {
+                    if (this->trie->search(s.substr(i,j+1)) && dfs(j+1))
+                        return true;
+                }
+            }
+            return false;
+        };
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<string> v;
-        string s;
-        Node *root = new Node();
-        insert(root, s);
-        FOR(i, 0, v.size()) { search(root, wordDict[i]); }
+        Trie* obj = new Trie();
+        this->s = s;
+        for (auto word:wordDict){obj->insert(word);}
+        dfs(0);        
     }
 };
 // @lc code=end
