63445be398fb4a4664f33ad59786a43b408fd2d4
DanielDu <danghoangnhan.1@gmail.com>
2023-03-15 11:27:07 +0800

    complete 70 (browser)

---
+++ 70.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int ways[302];
+        if(n <=2){return n;}
+        ways[0]=0;
+        ways[1]=1;
+        ways[2]=2;
+        for(int i=3;i<=n;i++){
+            ways[i]=ways[i-1]+ways[i-2];
+
+        }
+        return ways[n];
+    }
+};
