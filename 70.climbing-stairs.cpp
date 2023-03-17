70e8d46f564ee9e4abfb141dab168903cf62de35
Danieldu <daniel@jasslin.com>
2023-03-18 02:08:49 +0800

    fix conflict

--- 70.climbing-stairs.cpp
+++ 70.climbing-stairs.cpp
@@ -10,6 +10,33 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
+        if (n<=2){    return n;}
+        vector<int> steps;
+        steps[0]=0;
+        steps[1]=1;
+        steps[2]=2;
+        for (int i = 3; i < n+1; i++)
+        {
+            steps[i]=steps[i-1];
+            if (i%2==0){steps[i]++;}
+            
+        }
+        
+        
+    }
+};
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int climbStairs(int n) {
         int ways[302];
         if(n <=2){return n;}
         ways[0]=0;
