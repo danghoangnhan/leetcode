5fe81cf7864057ad65b40edca0ba6d2b93da86f4
danieldu <danghoangnhan.1@gmail.com>
2023-03-21 15:12:39 +0800

    add 53

--- 53.maximum-subarray.cpp
+++ 53.maximum-subarray.cpp
@@ -11,35 +11,17 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    int maps[100000];
     int maxSubArray(vector<int>& nums) {
-        int result = INT_MIN;
-        this->maps[0]=nums[0];
         for (int i = 1; i < nums.size(); i++){
-            this->maps[i] = this->maps[i-1]+nums[i];
+            nums[i] = max(nums[i],nums[i-1]+nums[i]);
         }
+        int result = nums[0];
         for (int i = 1; i < nums.size(); i++)
         {
-            result = max(result,this->maps[i]);
-            for (int j = 0; j<i; j++){
-                int data = this->maps[i]-this->maps[j];
-                cout<<data<<endl;
-                result = max(result,data);
-            }
+            result = max(nums[i],result);
         }
         return result;
     }
 };
-int main(){
-    Solution solution;
-    vector<int> data = {5,4,-1,7,8};
-    cout << solution.maxSubArray(data) <<endl;
-    for (int i = 0; i < data.size(); i++)
-    {
-        cout<<solution.maps[i]<<" ";
-    }
-    
-    return 0;
-}
 // @lc code=end
 
