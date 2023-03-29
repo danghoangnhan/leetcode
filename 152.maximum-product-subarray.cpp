222f2fb1ff6bf79aa197fdcdcc4d3e06a51f32fc
Danieldu <daniel@jasslin.com>
2023-03-29 13:44:34 +0800

    reconsrtruct

---
+++ 152.maximum-product-subarray.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+/*
+ * @lc app=leetcode id=152 lang=cpp
+ *
+ * [152] Maximum Product Subarray
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        vector<int>findMaxMul(nums.size(),0);
+        vector<int>mulRange(nums.size(),0);
+        result[0] = nums[0];
+        mulRange[0] =nums[0];
+    for(int i =0;i<nums.size()i++){
+
+    }
+    }
+};
+// @lc code=end
+
