75c7b1be2bec55a6225fd9b27c591a2737df0022
Danieldu <daniel@jasslin.com>
2023-04-09 01:04:07 +0800

    add leetcode stats

--- 221.maximal-square.cpp
+++ 221.maximal-square.cpp
@@ -10,26 +10,37 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    int helper(int row,int column,bool matrixIndex){
+    int helper(int row,int column){
         if (row>=this->rows ||column>=this->cols){return 0;}
-        pair<int,int> currentIndex = make_pair(row,column);
-                if (this->cache.count(currentIndex)){
+                if (this->cache[row][column]==-1){
                     int down = helper(row+1,column);
                     int right = helper(row,column+1);
                     int diag = helper(row+1,column+1);
-                    this->cache.emplace(currentIndex,(matrixIndex)? 1 + min(down,min(right,diag)):0);
+                    this->cache[row][column]=(this->matrix[row][column]=='1')? 1 + min(down,min(right,diag)):0;
                 }
-        return cache[currentIndex];
+        return this->cache[row][column];
+    }
+    int maximum(vector<vector<int>>& matrix){
+        int result = matrix[0][0];
+        for (int i = 0; i < matrix.size(); i++){
+            for (int j = 0; j < matrix[i].size(); j++){
+                result = max(result,matrix[i][j]);
+            }
+        }
+        return result;
     }
     int maximalSquare(vector<vector<char>>& matrix) {
         this->cols = matrix[0].size();
         this->rows = matrix.size();
-        helper(0,0);   
-        return max(cache.values())**2;
+        this->matrix = matrix;
+        this->cache = new vector(rows,vector<int>(cols,-1));
+        helper(0,0);
+        int value = maximum(this->cache);   
+        return value * value;
     }
     private:
         int cols,rows;
-        map<pair<int,int>,int> cache;
+        vector<vector<int>>* cache;
+        vector<vector<char>>& matrix;
 };
 // @lc code=end
-
