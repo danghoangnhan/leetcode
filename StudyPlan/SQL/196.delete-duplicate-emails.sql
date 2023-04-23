0629a7742682149527953a2a9df0149b226d395d
Danieldu <daniel@jasslin.com>
2023-04-24 02:50:10 +0800

    init ProgrammingSkill

--- StudyPlan/SQL/196.delete-duplicate-emails.sql
+++ StudyPlan/SQL/196.delete-duplicate-emails.sql
@@ -7,8 +7,14 @@
 -- @lc code=start
 # Please write a DELETE statement and DO NOT write a SELECT statement.
 # Write your MySQL query statement below
-DELETE p1
-FROM Person p1
-JOIN Person p2 ON p1.email = p2.email AND p1.id > p2.id;
+DELETE FROM Person
+WHERE (email, id) IN (
+    SELECT email, id
+    FROM (
+        SELECT email, id, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS rn
+        FROM Person
+    ) AS subquery
+    WHERE rn > 1
+);
 -- @lc code=end
 
