46d718f08697982f72ea8091b96703fc7d73f38d
Danieldu <daniel@jasslin.com>
2024-05-31 12:28:51 +0800

    add 200

--- 200.number-of-islands.cpp
+++ 200.number-of-islands.cpp
@@ -5,10 +5,33 @@
  */
 
 // @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
+    void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& visited) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size() || visited[i][j] || grid[i][j] == '0') {
+            return;
+        }
+        visited[i][j] = true;
+        dfs(grid, i - 1, j, visited);
+        dfs(grid, i + 1, j, visited);
+        dfs(grid, i, j - 1, visited);
+        dfs(grid, i, j + 1, visited);
+    }
     int numIslands(vector<vector<char>>& grid) {
-        
+        int result = 0;
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    result++;
+                    dfs(grid, i, j, visited);
+                }
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
