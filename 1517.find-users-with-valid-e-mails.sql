f345aaa0598e79025bfa071bcbb3f6f8a075dc3a
Danieldu <daniel@jasslin.com>
2024-08-02 09:57:19 +0800

    add 1517

---
+++ 1517.find-users-with-valid-e-mails.sql
@@ -0,0 +1,18 @@
+--
+-- @lc app=leetcode id=1517 lang=mysql
+--
+-- [1517] Find Users With Valid E-Mails
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT 
+    user_id, 
+    name, 
+    mail
+FROM 
+    Users
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$';
+-- @lc code=end
+
