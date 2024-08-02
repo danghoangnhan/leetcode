d47a5030c7f0ef248778915759b55b99c0cca100
Danieldu <daniel@jasslin.com>
2024-08-02 10:11:21 +0800

    update sql 50

---
+++ StudyPlan/SQL50/1070.product-sales-analysis-iii.sql
@@ -0,0 +1,14 @@
+--
+-- @lc app=leetcode id=1070 lang=mysql
+--
+-- [1070] Product Sales Analysis III
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select product_id, year as first_year, quantity, price
+from Sales
+where (product_id, year) in (select product_id, min(year) as year from Sales
+group by product_id);
+-- @lc code=end
+
