d0b5bc7706e1b1154fa4cb0bba5ebd8de8316e5a
Danieldu <daniel@jasslin.com>
2024-08-05 20:59:22 +0800

    update sql 50

--- StudyPlan/SQL50/1070.product-sales-analysis-iii.sql
+++ StudyPlan/SQL50/1070.product-sales-analysis-iii.sql
@@ -9,6 +9,10 @@
 select product_id, year as first_year, quantity, price
 from Sales
 where (product_id, year) in (select product_id, min(year) as year from Sales
+group by product_id);year) in (select product_id, min(year) as year from Sales
+group by product_id);select product_id, year as first_year, quantity, price
+from Sales
+where (product_id, year) in (select product_id, min(year) as year from Sales
 group by product_id);
 -- @lc code=end
 
