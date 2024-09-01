8d00a69015ba76e36bdc8a4992d216bb6d43c2f8
Danieldu <daniel@jasslin.com>
2024-09-01 16:14:23 +0800

    add 2022

---
+++ 2022.convert-1-d-array-into-2-d-array.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=2022 lang=cpp
+ *
+ * [2022] Convert 1D Array Into 2D Array
+ */
+
+// @lc code=start
+#include "bits/stdc++.h"
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if (original.size() != m * n) {
+            return {};  // Return empty vector if sizes don't match
+        }
+        
+        vector<vector<int>> result(m, vector<int>(n));
+        for (int i = 0; i < original.size(); ++i) {
+            result[i / n][i % n] = original[i];
+        }
+        
+        return result;
+    }
+};
+// @lc code=end
+
