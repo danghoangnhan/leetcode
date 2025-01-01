334842ca37852ccf0cfd2e3aa7f65a53e9ac6d9b
Danieldu <daniel@jasslin.com>
2025-01-01 17:25:41 +0800

    add 66

---
+++ 66.plus-one.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=66 lang=cpp
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        int index = digits.size()-1;
+        while(index>=0 && subber>0){
+            digits[index]   +=  subber;
+            carry = digits[index]/10;
+            digits[index] = digits[index]%10;
+            index--;
+        }
+        if(carry>0){
+            digits.insert(digits.begin(), carry);
+        }
+        return digits;
+    }
+};
+// @lc code=end
+
