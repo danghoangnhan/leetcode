754f410f27b31803e08a47cdfdc7a385597ee43f
Danieldu <daniel@jasslin.com>
2025-01-18 16:21:54 +0800

    add 496

--- 496.next-greater-element-i.cpp
+++ 496.next-greater-element-i.cpp
@@ -5,12 +5,35 @@
  */
 
 // @lc code=start
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
+
+        unordered_map<int, int> next_greater;
+        stack<int> st;
+    
+        for (int num : nums2) {
+            while (!st.empty() && num > st.top()) {
+                next_greater[st.top()] = num;
+                st.pop();
+            }
+            st.push(num);
+        }
+    
+        while (!st.empty()) {
+            next_greater[st.top()] = -1;
+            st.pop();
+        }
+    
+        vector<int> result;
+        result.reserve(nums1.size());
+        for (int x : nums1) {
+            result.push_back(next_greater[x]);
+        }
+    return result;
     }
 };
 // @lc code=end
