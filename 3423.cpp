2f7480703c7796aca594de0f7d20e37e42483d92
Danieldu <daniel@jasslin.com>
2025-01-19 13:19:08 +0800

    add 3423

---
+++ 3423.cpp
@@ -0,0 +1,15 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution {
+public:
+    int maxAdjacentDistance(vector<int>& nums) {
+    int n = nums.size();
+    int max_diff = 0;
+
+    for(int i = 0; i < n; i++)
+        max_diff = max(max_diff, abs(nums[i] - nums[(i + 1) % n]));
+    
+    return max_diff;
+    }
+};
