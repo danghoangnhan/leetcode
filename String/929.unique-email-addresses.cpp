d76507a5d57f794fe0f9dbe03db73f59a41d21df
Danieldu <daniel@jasslin.com>
2025-02-19 00:09:47 +0800

    init tree topic

---
+++ String/929.unique-email-addresses.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=929 lang=cpp
+ *
+ * [929] Unique Email Addresses
+ */
+
+// @lc code=start
+#include "bits/stdc++.h"
+using namespace std;
+
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        set<string> check_list;
+        for(auto& email:emails){
+            vector<string> names = split(email,'@');
+        }
+        return check_list.size();
+    }
+};
+// @lc code=end
+
